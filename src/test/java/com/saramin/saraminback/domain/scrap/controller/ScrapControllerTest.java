f20b2147fbb239ece3a75129695018233e68fe6e
kho903 <gmldnr2222@naver.com>
2022-02-22 19:25:15 +0900

    test: getExperience

--- src/test/java/com/saramin/saraminback/domain/scrap/controller/ScrapControllerTest.java
+++ src/test/java/com/saramin/saraminback/domain/scrap/controller/ScrapControllerTest.java
@@ -205,4 +205,129 @@ class ScrapControllerTest extends TestSupport {
 					fieldWithPath("[0].close-type.name").description("마감일 값\n"))
 			));
 	}
+
+	@Test
+	void getExperience() throws Exception {
+		mockMvc.perform(
+			get("/apis/scrap/experience")
+				.contentType(MediaType.APPLICATION_JSON)
+		)
+			.andExpect(status().isOk())
+			.andDo(
+				restDocs.document(
+					responseFields(
+						fieldWithPath("experience").description("경력"),
+						fieldWithPath("new_experience").description("신입"),
+						fieldWithPath("both_experience").description("신입 / 경력"),
+						fieldWithPath("any_experience").description("경력 무관"),
+						fieldWithPath("experience[0].url").description("채용공고 표준 URL. 공채속보의 경우, 리디렉션이 있을 수 있습니다."),
+						fieldWithPath("experience[0].active").description("공고 진행 여부 1: 진행중, 0: 마감"),
+						fieldWithPath("experience[0].company").description("공고 회사"),
+						fieldWithPath("experience[0].company.detail").description("공고 회사 상세 정보"),
+						fieldWithPath("experience[0].company.detail.href").description("기업정보 페이지 (공개되어 있는 경우)"),
+						fieldWithPath("experience[0].company.detail.name").description("공고 회사 이름"),
+						fieldWithPath("experience[0].position").description("채용 포지션"),
+						fieldWithPath("experience[0].position.title").description("공고 제목"),
+						fieldWithPath("experience[0].position.industry").description("업종"),
+						fieldWithPath("experience[0].position.industry.code").description("업종 코드\n"),
+						fieldWithPath("experience[0].position.industry.name").description("업종명\n"),
+						fieldWithPath("experience[0].position.location").description("지역\n"),
+						fieldWithPath("experience[0].position.location.code").description("지역 코드\n"),
+						fieldWithPath("experience[0].position.location.name").description("지역명"),
+						fieldWithPath("experience[0].position.job-type").description("근무형태"),
+						fieldWithPath("experience[0].position.job-type.code").description("근무형태 코드\n"),
+						fieldWithPath("experience[0].position.job-type.name").description("근무형태 값\n"),
+						fieldWithPath("experience[0].position.job-mid-code").description("상위 직무\n"),
+						fieldWithPath("experience[0].position.job-mid-code.code").description("상위 직무 코드\n"),
+						fieldWithPath("experience[0].position.job-mid-code.name").description("상위 직무명\n"),
+						fieldWithPath("experience[0].position.job-code").description("직무"),
+						fieldWithPath("experience[0].position.job-code.code").description("직종 코드\n"),
+						fieldWithPath("experience[0].position.job-code.name").description("직무명\n"),
+						fieldWithPath("experience[0].position.experience-level").description("경력\n"),
+						fieldWithPath("experience[0].position.experience-level.code").description("경력 코드\n"
+							+ "1 : 신입\n"
+							+ "2 : 경력\n"
+							+ "3 : 신입/경력\n"
+							+ "0 : 경력무관"),
+						fieldWithPath("experience[0].position.experience-level.min").description("경력 최소 값\n"),
+						fieldWithPath("experience[0].position.experience-level.max").description("경력 최대 값\n"),
+						fieldWithPath("experience[0].position.experience-level.name").description("경력 값\n"),
+						fieldWithPath("experience[0].position.required-education-level").description("학력"),
+						fieldWithPath("experience[0].position.required-education-level.code").description("학력 코드\n"),
+						fieldWithPath("experience[0].position.required-education-level.name").description("학력 값\n"),
+						fieldWithPath("experience[0].keyword").description("키워드\n"),
+						fieldWithPath("experience[0].salary").description("연봉"),
+						fieldWithPath("experience[0].salary.code").description("연봉 코드\n"),
+						fieldWithPath("experience[0].salary.name").description("연봉 값\n"),
+						fieldWithPath("experience[0].id").description("공고 번호\n"),
+						fieldWithPath("experience[0].posting-timestamp").description("게시일의 Unix timestamp\n"),
+						fieldWithPath("experience[0].modification-timestamp").description("수정일 Unix timestamp\n"),
+						fieldWithPath("experience[0].opening-timestamp").description("접수 시작일의 Unix timestamp\n"),
+						fieldWithPath("experience[0].expiration-timestamp").description("마감일의 Unix timestamp\n"),
+						fieldWithPath("experience[0].close-type").description("마감일 형식"),
+						fieldWithPath("experience[0].close-type.code").description("마감일 코드\n"
+							+ "1 : 접수 마감일\n"
+							+ "2 : 채용시\n"
+							+ "3 : 상시\n"
+							+ "4 : 수시"),
+						fieldWithPath("experience[0].close-type.name").description("마감일 값\n"),
+
+
+						fieldWithPath("both_experience[0].url").description("채용공고 표준 URL. 공채속보의 경우, 리디렉션이 있을 수 있습니다."),
+						fieldWithPath("both_experience[0].active").description("공고 진행 여부 1: 진행중, 0: 마감"),
+						fieldWithPath("both_experience[0].company").description("공고 회사"),
+						fieldWithPath("both_experience[0].company.detail").description("공고 회사 상세 정보"),
+						fieldWithPath("both_experience[0].company.detail.href").description("기업정보 페이지 (공개되어 있는 경우)"),
+						fieldWithPath("both_experience[0].company.detail.name").description("공고 회사 이름"),
+						fieldWithPath("both_experience[0].position").description("채용 포지션"),
+						fieldWithPath("both_experience[0].position.title").description("공고 제목"),
+						fieldWithPath("both_experience[0].position.industry").description("업종"),
+						fieldWithPath("both_experience[0].position.industry.code").description("업종 코드\n"),
+						fieldWithPath("both_experience[0].position.industry.name").description("업종명\n"),
+						fieldWithPath("both_experience[0].position.location").description("지역\n"),
+						fieldWithPath("both_experience[0].position.location.code").description("지역 코드\n"),
+						fieldWithPath("both_experience[0].position.location.name").description("지역명"),
+						fieldWithPath("both_experience[0].position.job-type").description("근무형태"),
+						fieldWithPath("both_experience[0].position.job-type.code").description("근무형태 코드\n"),
+						fieldWithPath("both_experience[0].position.job-type.name").description("근무형태 값\n"),
+						fieldWithPath("both_experience[0].position.job-mid-code").description("상위 직무\n"),
+						fieldWithPath("both_experience[0].position.job-mid-code.code").description("상위 직무 코드\n"),
+						fieldWithPath("both_experience[0].position.job-mid-code.name").description("상위 직무명\n"),
+						fieldWithPath("both_experience[0].position.job-code").description("직무"),
+						fieldWithPath("both_experience[0].position.job-code.code").description("직종 코드\n"),
+						fieldWithPath("both_experience[0].position.job-code.name").description("직무명\n"),
+						fieldWithPath("both_experience[0].position.experience-level").description("경력\n"),
+						fieldWithPath("both_experience[0].position.experience-level.code").description("경력 코드\n"
+							+ "1 : 신입\n"
+							+ "2 : 경력\n"
+							+ "3 : 신입/경력\n"
+							+ "0 : 경력무관"),
+						fieldWithPath("both_experience[0].position.experience-level.min").description("경력 최소 값\n"),
+						fieldWithPath("both_experience[0].position.experience-level.max").description("경력 최대 값\n"),
+						fieldWithPath("both_experience[0].position.experience-level.name").description("경력 값\n"),
+						fieldWithPath("both_experience[0].position.required-education-level").description("학력"),
+						fieldWithPath("both_experience[0].position.required-education-level.code").description("학력 코드\n"),
+						fieldWithPath("both_experience[0].position.required-education-level.name").description("학력 값\n"),
+						fieldWithPath("both_experience[0].keyword").description("키워드\n"),
+						fieldWithPath("both_experience[0].salary").description("연봉"),
+						fieldWithPath("both_experience[0].salary.code").description("연봉 코드\n"),
+						fieldWithPath("both_experience[0].salary.name").description("연봉 값\n"),
+						fieldWithPath("both_experience[0].id").description("공고 번호\n"),
+						fieldWithPath("both_experience[0].posting-timestamp").description("게시일의 Unix timestamp\n"),
+						fieldWithPath("both_experience[0].modification-timestamp").description("수정일 Unix timestamp\n"),
+						fieldWithPath("both_experience[0].opening-timestamp").description("접수 시작일의 Unix timestamp\n"),
+						fieldWithPath("both_experience[0].expiration-timestamp").description("마감일의 Unix timestamp\n"),
+						fieldWithPath("both_experience[0].close-type").description("마감일 형식"),
+						fieldWithPath("both_experience[0].close-type.code").description("마감일 코드\n"
+							+ "1 : 접수 마감일\n"
+							+ "2 : 채용시\n"
+							+ "3 : 상시\n"
+							+ "4 : 수시"),
+						fieldWithPath("both_experience[0].close-type.name").description("마감일 값\n")
+					)
+
+				)
+			)
+		;
+	}
 }
